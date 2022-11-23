53900c8eff6c18c7dffdf11b96d7cd7783436336
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-23 20:49:59 +0100

    Add a random auto color mode

--- java.js
+++ java.js
@@ -1,16 +1,32 @@
 const colorButton = document.getElementById('random-button');
 const colorDisplayedText = document.getElementById('color-displayed');
 const wholePage = document.getElementById('whole-page');
+const autoButton = document.getElementById('auto-button');
+let interval = null;
 
 colorButton.addEventListener('click', () => {
-    generateRandomColor ();
+    generateRandomColor();
     wholePage.style.backgroundColor = backgroundColor;
+    colorDisplayedText.textContent = backgroundColor;
+})
+
+autoButton.addEventListener('click', () => {
+    if (interval === null) {
+        interval = setInterval(() => {
+            generateRandomColor();
+            wholePage.style.backgroundColor = backgroundColor;
+            colorDisplayedText.textContent = backgroundColor;
+        }, 1000)
+    } else {
+        clearInterval(interval);
+        interval = null;
+    }
 })
 
 function generateRandomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-    console.log (r, g, b)
+    console.log(r, g, b)
     return backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 }
