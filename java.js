710ee5e83141a4d53606a9733978ce931b207be2
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-11-27 18:45:15 +0100

    Create a text color changer and connect it to the new button

--- java.js
+++ java.js
@@ -2,12 +2,16 @@ const colorButton = document.getElementById('random-button');
 const colorDisplayedText = document.getElementById('color-displayed');
 const wholePage = document.getElementById('whole-page');
 const autoButton = document.getElementById('auto-button');
+const combineButton = document.getElementById('combine-colors');
+const mainText = document.getElementById('main-text');
 let interval = null;
 
 colorButton.addEventListener('click', () => {
     generateRandomColor();
     wholePage.style.backgroundColor = backgroundColor;
     colorDisplayedText.textContent = backgroundColor;
+    clearInterval(interval);
+    interval = null;
 })
 
 autoButton.addEventListener('click', () => {
@@ -23,10 +27,22 @@ autoButton.addEventListener('click', () => {
     }
 })
 
+combineButton.addEventListener('click', () => {
+    generateRandomColor2();
+    mainText.style.color = backgroundColor2;
+    colorDisplayedText.style.color = backgroundColor2;
+})
+
 function generateRandomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-    console.log(r, g, b)
     return backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function generateRandomColor2() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return backgroundColor2 = 'rgb(' + r + ',' + g + ',' + b + ')';
 }
